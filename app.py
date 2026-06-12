06d3c997a8e266c65b4a8c5c52f03004f82b4a4b
Shravani Mugalikar <shravani.mugalikar@gmail.com>
2026-06-12 16:32:20 +0530

    Initial DevOps capstone

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Welcome to DevOps Pulse"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
